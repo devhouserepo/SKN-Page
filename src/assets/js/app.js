680ecf49913ee842cd7c5e314eef16e56bc296f8
pio-kniaz <pio.kniaz@gmail.com>
2018-03-16 01:11:01 +0100

    form , MQ mixins , modal added

---
+++ src/assets/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("#click").click(function() {
+        $("#form").slideToggle("slow");
+        $("#form2").slideToggle("slow");
+    });
+
+    $("#click2").click(function() {
+        $("#form").slideToggle("slow");
+        $("#form2").slideToggle("slow");
+    });
+
+    $("#myModal").on("shown.bs.modal", function() {
+        $("#myInput").trigger("focus");
+    });
+});
