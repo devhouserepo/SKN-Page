103a70455f5b8db66293190582ebc56785ea2f7e
Aleks <aleks.grunwald@gmail.com>
2018-03-23 02:32:50 +0100

    Show and hide show-more-about-project button written in javascript. Started adding svg bckg

--- src/temp/js/app.js
+++ src/temp/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     $("#click").click(function() {
         $("#form").slideToggle("slow");
         $("#form2").slideToggle("slow");
@@ -12,4 +13,25 @@ $(document).ready(function() {
     $("#myModal").on("shown.bs.modal", function() {
         $("#myInput").trigger("focus");
     });
+
+
+
+    // Projects section
+
+    $(".button-more-about-project").hide
+
+    $(".projects__box, .projects__box2").mouseover(function() {
+        setTimeout(function() {
+            $(".button-more-about-project").show
+        }, 1800); 
+    })
+
+    $(".projects__box, .projects__box2").mouseout(function() {
+        setTimeout(function() {
+            $(".button-more-about-project").hide
+        }, 1800); 
+    })
+
+    // Project section ends
+
 });
