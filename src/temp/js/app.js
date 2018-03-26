c58a86db9a02eaa19b227f8e23201bbebb27a15a
Aleks <aleks.grunwald@gmail.com>
2018-03-26 15:55:34 +0200

    Editing twoj-projekt

--- src/temp/js/app.js
+++ src/temp/js/app.js
@@ -19,27 +19,6 @@ $(document).ready(function () {
     // Projects section
 
 
-    // $(".singular-project-container").mouseenter(function(e) {
-
-    //      let expandSingularProjectContainer = {
-    //         'width': '400px',
-    //         'height' : '400px',
-    //         'z-index': 3
-    //     }
-    
-    //     $(this).children(".projects-shape-1, .projects-shape-2").css(expandSingularProjectContainer)
-
-    //     let currentTarget = e.currentTarget
-    //     setTimeout(function(target) {       
-    //         $(target).find(".project-about").css("opacity", "1")
-    //     }, 700, currentTarget)
-    // })
-
-
-    // $(".singular-project-container").mouseleave(function(e) {
-
-    // })
-
 
     // Project section ends
 });
