44f85fe3710172b66172cba8e71a367e7ea32147
Aleks <aleks.grunwald@gmail.com>
2018-03-26 13:48:52 +0200

    project box expanding by css animation

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -19,32 +19,26 @@ $(document).ready(function () {
     // Projects section
 
 
-    $(".singular-project-container").mouseenter(function(e) {
-        console.log($(".singular-project-container"))
-         let expandSingularProjectContainer = {
-            'width': '400px',
-            'height' : '400px',
-            'z-index': 3
-        }
+    // $(".singular-project-container").mouseenter(function(e) {
+
+    //      let expandSingularProjectContainer = {
+    //         'width': '400px',
+    //         'height' : '400px',
+    //         'z-index': 3
+    //     }
     
-        $(this).children(".projects-shape-1, .projects-shape-2").css(expandSingularProjectContainer)
+    //     $(this).children(".projects-shape-1, .projects-shape-2").css(expandSingularProjectContainer)
 
-        // let currentTarget = e.currentTarget
-        // setTimeout(function(target) {       
-        //     $(target).find(".project-about").css("opacity", "1")
-        // }, 700, currentTarget)
-    })
+    //     let currentTarget = e.currentTarget
+    //     setTimeout(function(target) {       
+    //         $(target).find(".project-about").css("opacity", "1")
+    //     }, 700, currentTarget)
+    // })
 
 
-    $(".singular-project-container").mouseleave(function(e) {
-        let collapseSingularProjectContainer = {
-            'width': '200px',
-            'height' : '200px',
-            'z-index': 2
-        }
+    // $(".singular-project-container").mouseleave(function(e) {
 
-        $(this).children(".projects-shape-1, .projects-shape-2").css(collapseSingularProjectContainer)
-    })
+    // })
 
 
     // Project section ends
