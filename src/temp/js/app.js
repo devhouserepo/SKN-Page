986d53207c0916312c488a0e10d296ef7b740b84
Aleks <aleks.grunwald@gmail.com>
2018-06-07 16:52:09 +0200

    SISK-34-grid -- Projects titels from JSON.

--- src/temp/js/app.js
+++ src/temp/js/app.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
   //Checking which element overflows the document
   var docWidth = document.documentElement.offsetWidth;
 
   [].forEach.call(
     document.querySelectorAll('*'),
-    function(el) {
+    function (el) {
       if (el.offsetWidth > docWidth) {
         console.log(el);
       }
@@ -15,9 +15,9 @@ $(document).ready(function() {
 
   let number = 0;
 
-  $("#private-button").click(function() {
+  $("#private-button").click(function () {
     if (number === 2) {
-      setTimeout(function() {
+      setTimeout(function () {
         document.getElementsByName("formSchool")[0].placeholder = "Uczelnia";
         document.getElementsByName("formDepartment")[0].placeholder = "Wydział";
       }, 0600);
@@ -28,9 +28,9 @@ $(document).ready(function() {
     $("#form-contact, #form-content").slideToggle(600);
   });
 
-  $("#company-button").click(function() {
+  $("#company-button").click(function () {
     if (number === 1) {
-      setTimeout(function() {
+      setTimeout(function () {
         document.getElementsByName("formSchool")[0].placeholder = "Firma";
         document.getElementsByName("formDepartment")[0].placeholder = "Dział";
       }, 0600);
@@ -44,11 +44,11 @@ $(document).ready(function() {
     $("#form-contact, #form-content").slideToggle(600);
   });
 
-  $("#myModal").on("shown.bs.modal", function() {
+  $("#myModal").on("shown.bs.modal", function () {
     $("#myInput").trigger("focus");
   });
 
-  $("div").on("click", function() {
+  $("div").on("click", function () {
     var target = $(this).attr("rel");
 
     $("#" + target)
@@ -61,40 +61,56 @@ $(document).ready(function() {
   //PROJECTS section START
 
 
-  $('.single-container').on('touchstart', function() {
-    $('.mobile-project-modal').css({
-      'opacity': '1',
-      'z-index': '10'})
-  })
 
-  $('.close-icon-svg').on('touchstart', function() {
-    $('.mobile-project-modal').css('opacity', '0')
-  })
+  function displayProjectInfoFromJSON() {
+    $.getJSON("../../projects-info/projects-info.json", function (data) {
+      $('.single-container__project-paragraphs__project-title').each(function (index) {
+        $(this).text(`${data[index].title}`)
+        // console.log(`$this: ${this}, title: ${data[index].title}`)
+      })
+    })
+  }
+
+  displayProjectInfoFromJSON()
+
+
+
+
+  $('.single-container').on('touchstart', function () {
+        $('.mobile-project-modal').css({
+          'opacity': '1',
+          'z-index': '10'
+        })
+      })
+
+  $('.close-icon-svg').on('touchstart', function () {
+        $('.mobile-project-modal').css('opacity', '0')
+      })
 
 
   // TEAM section START
 
   let teamDevhouse = "#team-devhouse";
-  let teamStartUp = "#team-startup";
-  let teamLEAD = "#team-lead";
-  let teamThinkIT = "#team-thinkit";
-  let teamBootcamp = "#team-bootcamp";
+    let teamStartUp = "#team-startup";
+    let teamLEAD = "#team-lead";
+    let teamThinkIT = "#team-thinkit";
+    let teamBootcamp = "#team-bootcamp";
 
 
-  function highlighSelectedProject(elementId) {
-    $(elementId).addClass("selected-project");
-  }
+    function highlighSelectedProject(elementId) {
+      $(elementId).addClass("selected-project");
+    }
 
 
-  function getDevhouseJSON() {
-    $.getJSON("../../skn-members/devhouse.json", function(data) {
-      const props = [...data]
+    function getDevhouseJSON() {
+      $.getJSON("../../skn-members/devhouse.json", function (data) {
+        const props = [...data]
         randomNumber = Math.floor(Math.random() * data.length)
         rndPerson(props);
-      
-      $.each(data, function(index, item) {
-        $(".members-thumbnails-container").append(
-          `<div class="flex-column align-items-center one-thumbnail-container">
+
+        $.each(data, function (index, item) {
+          $(".members-thumbnails-container").append(
+            `<div class="flex-column align-items-center one-thumbnail-container">
                     <img class="thumb-img"></img>
                     <p class="thumb-name-surname">` +
             data[index].first_name +
@@ -102,27 +118,27 @@ $(document).ready(function() {
             data[index].last_name +
             `</p>
                     </div>`
-        );
-        $(".thumb-img")
-          .eq(index)
-          .attr({
-            id: data[index].first_name + data[index].last_name,
-            alt:
-              data[index].first_name +
-              " " +
-              data[index].last_name +
-              ". Zdjęcie.",
-            src: data[index].photo
-          })
-          .click(function() {
-            $(".max-member-paragraphs").empty();
-            $(".wybierz-osobe").remove();
-            $(".max-photo").css(
-              "background-image",
-              "url(" + data[index].photo + ")"
-            );
-            $(".max-member-paragraphs").append(
-              `<p class="max-name-surname">` +
+          );
+          $(".thumb-img")
+            .eq(index)
+            .attr({
+              id: data[index].first_name + data[index].last_name,
+              alt:
+                data[index].first_name +
+                " " +
+                data[index].last_name +
+                ". Zdjęcie.",
+              src: data[index].photo
+            })
+            .click(function () {
+              $(".max-member-paragraphs").empty();
+              $(".wybierz-osobe").remove();
+              $(".max-photo").css(
+                "background-image",
+                "url(" + data[index].photo + ")"
+              );
+              $(".max-member-paragraphs").append(
+                `<p class="max-name-surname">` +
                 data[index].first_name +
                 ` ` +
                 data[index].last_name +
@@ -130,38 +146,38 @@ $(document).ready(function() {
                         <p class="max-member-info-details">` +
                 data[index].info +
                 `</p>`
-            );
-            if (data[index].linkedin != "") {
-              $(".max-member-paragraphs").append(
-                `<a class="linkedin-btn" target="_blank" href="` +
+              );
+              if (data[index].linkedin != "") {
+                $(".max-member-paragraphs").append(
+                  `<a class="linkedin-btn" target="_blank" href="` +
                   data[index].linkedin +
                   `"><img src="../assets/images/linkedin_white.png" width="80px"></a>`
-              );
-            }
-          });
+                );
+              }
+            });
+        });
       });
-    });
-  }
+    }
 
-  getDevhouseJSON();
-
-  highlighSelectedProject(teamDevhouse);
-
-  $("#team-devhouse").click(function() {
-    $(".members-thumbnails-container").empty();
-    $(".selected-project").toggleClass("selected-project");
-    highlighSelectedProject(teamDevhouse);
     getDevhouseJSON();
-  });
 
-  $("#team-startup").click(function() {
-    $(".members-thumbnails-container").empty();
-    $(".selected-project").toggleClass("selected-project");
-    highlighSelectedProject(teamStartUp);
-    $.getJSON("../../skn-members/startup.json", function(data) {
-      $.each(data, function(index, item) {
-        $(".members-thumbnails-container").append(
-          `<div class="flex-column align-items-center one-thumbnail-container">
+    highlighSelectedProject(teamDevhouse);
+
+    $("#team-devhouse").click(function () {
+      $(".members-thumbnails-container").empty();
+      $(".selected-project").toggleClass("selected-project");
+      highlighSelectedProject(teamDevhouse);
+      getDevhouseJSON();
+    });
+
+    $("#team-startup").click(function () {
+      $(".members-thumbnails-container").empty();
+      $(".selected-project").toggleClass("selected-project");
+      highlighSelectedProject(teamStartUp);
+      $.getJSON("../../skn-members/startup.json", function (data) {
+        $.each(data, function (index, item) {
+          $(".members-thumbnails-container").append(
+            `<div class="flex-column align-items-center one-thumbnail-container">
                         <img class="thumb-img"></img>
                         <p class="thumb-name-surname">` +
             data[index].first_name +
@@ -169,27 +185,27 @@ $(document).ready(function() {
             data[index].last_name +
             `</p>
                         </div>`
-        );
-        $(".thumb-img")
-          .eq(index)
-          .attr({
-            id: data[index].first_name + data[index].last_name,
-            alt:
-              data[index].first_name +
-              " " +
-              data[index].last_name +
-              ". Zdjęcie.",
-            src: data[index].photo
-          })
-          .click(function() {
-            $(".max-member-paragraphs").empty();
-            $(".wybierz-osobe").remove();
-            $(".max-photo").css(
-              "background-image",
-              "url(" + data[index].photo + ")"
-            );
-            $(".max-member-paragraphs").append(
-              `<p class="max-name-surname">` +
+          );
+          $(".thumb-img")
+            .eq(index)
+            .attr({
+              id: data[index].first_name + data[index].last_name,
+              alt:
+                data[index].first_name +
+                " " +
+                data[index].last_name +
+                ". Zdjęcie.",
+              src: data[index].photo
+            })
+            .click(function () {
+              $(".max-member-paragraphs").empty();
+              $(".wybierz-osobe").remove();
+              $(".max-photo").css(
+                "background-image",
+                "url(" + data[index].photo + ")"
+              );
+              $(".max-member-paragraphs").append(
+                `<p class="max-name-surname">` +
                 data[index].first_name +
                 ` ` +
                 data[index].last_name +
@@ -197,29 +213,29 @@ $(document).ready(function() {
                             <p class="max-member-info-details">` +
                 data[index].info +
                 `</p>`
-            );
-            if (data[index].linkedin != "") {
-              $(".max-member-paragraphs").append(
-                `<a class="linkedin-btn" href="` +
+              );
+              if (data[index].linkedin != "") {
+                $(".max-member-paragraphs").append(
+                  `<a class="linkedin-btn" href="` +
                   data[index].linkedin +
                   `"><img src="../assets/images/linkedin_white.png" width="80px"></a>`
-              );
-            }
-          });
+                );
+              }
+            });
+        });
       });
     });
-  });
 
-const rndPerson = (props) => {
-    //
-    $(".max-member-paragraphs").empty();
-    $(".wybierz-osobe").remove();
-    $(".max-photo").css(
-      "background-image",
-      "url(" + props[randomNumber].photo + ")"
-    );
-    $(".max-member-paragraphs").append(
-      `<p class="max-name-surname">` +
+    const rndPerson = (props) => {
+      //
+      $(".max-member-paragraphs").empty();
+      $(".wybierz-osobe").remove();
+      $(".max-photo").css(
+        "background-image",
+        "url(" + props[randomNumber].photo + ")"
+      );
+      $(".max-member-paragraphs").append(
+        `<p class="max-name-surname">` +
         props[randomNumber].first_name +
         ` ` +
         props[randomNumber].last_name +
@@ -227,62 +243,62 @@ const rndPerson = (props) => {
                     <p class="max-member-info-details">` +
         props[randomNumber].info +
         `</p>`
-    );
-    if (props[randomNumber].linkedin != "") {
-      $(".max-member-paragraphs").append(
-        `<a class="linkedin-btn" href="` +
+      );
+      if (props[randomNumber].linkedin != "") {
+        $(".max-member-paragraphs").append(
+          `<a class="linkedin-btn" href="` +
           props[randomNumber].linkedin +
           `" target="_blank"><img src="../assets/images/linkedin_white.png" width="80px"></a>`
-      );
+        );
+      }
+
     }
 
-  }
-
-  // TEAM section END
+    // TEAM section END
 
 
 
-  // $("#fullpage").fullpage({
-  //   anchors: [
-  //     "start",
-  //     "projekty",
-  //     "partnerzy",
-  //     "zespol",
-  //     "referencje",
-  //     "kontakt"
-  //   ],
-  //   responsiveWidth: 991,
-  //   sectionSelector: ".section",
-  //   slideSelector: ".slide2",
-  //   onLeave: function(index, nextIndex, direction) {
-  //     // console.log(index);
-  //     var leavingSection = $(this);
-  //     //after leaving section 1
-  //     if (index == 1) {
-  //       document.getElementById("navbar").classList.add("scrolled");
-  //     }
-  //     if (index == 2 && direction == "up") {
-  //       document.getElementById("navbar").classList.remove("scrolled");
-  //     }
-  //   },
-  //   afterLoad: function(anchor, index) {
-  //     if (index == 1) {
-  //       document.getElementById("navbar").classList.remove("scrolled");
-  //     } else if (index == 2) {
-  //       $(".svg-1").css("display", "block");
-  //       $(".svg-2").css("display", "block");
-  //     } else if (index == 5) {
-  //       $(".svg-3").css("display", "block");
-  //     } else if (index == 6) {
-  //       $(".svg-4").css("display", "block");
-  //     }
-  //     const anchorsLink = document.querySelectorAll(".navbar-nav li a");
-  //     // console.log(anchor, index, anchorsLink);
-  //     anchorsLink.forEach(elem => {
-  //       elem.classList.remove("active");
-  //     });
-  //     anchorsLink[index - 1].classList.add("active");
-  //   }
-  // });
+    // $("#fullpage").fullpage({
+    //   anchors: [
+    //     "start",
+    //     "projekty",
+    //     "partnerzy",
+    //     "zespol",
+    //     "referencje",
+    //     "kontakt"
+    //   ],
+    //   responsiveWidth: 991,
+    //   sectionSelector: ".section",
+    //   slideSelector: ".slide2",
+    //   onLeave: function(index, nextIndex, direction) {
+    //     // console.log(index);
+    //     var leavingSection = $(this);
+    //     //after leaving section 1
+    //     if (index == 1) {
+    //       document.getElementById("navbar").classList.add("scrolled");
+    //     }
+    //     if (index == 2 && direction == "up") {
+    //       document.getElementById("navbar").classList.remove("scrolled");
+    //     }
+    //   },
+    //   afterLoad: function(anchor, index) {
+    //     if (index == 1) {
+    //       document.getElementById("navbar").classList.remove("scrolled");
+    //     } else if (index == 2) {
+    //       $(".svg-1").css("display", "block");
+    //       $(".svg-2").css("display", "block");
+    //     } else if (index == 5) {
+    //       $(".svg-3").css("display", "block");
+    //     } else if (index == 6) {
+    //       $(".svg-4").css("display", "block");
+    //     }
+    //     const anchorsLink = document.querySelectorAll(".navbar-nav li a");
+    //     // console.log(anchor, index, anchorsLink);
+    //     anchorsLink.forEach(elem => {
+    //       elem.classList.remove("active");
+    //     });
+    //     anchorsLink[index - 1].classList.add("active");
+    //   }
+    // });
 
-});
+  });
